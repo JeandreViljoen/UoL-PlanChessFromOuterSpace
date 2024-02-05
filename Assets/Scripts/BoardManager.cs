fb23854fc536b092a990918570bc51dcf4a90c65
Nathan Du <nathandu@outlook.com>
2024-02-05 21:52:48 +0800

    add methods to retrieve and move pieces

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -10,7 +10,7 @@ public class BoardManager : MonoService
 {
     // --------------- Member variables and data --------------- //
     // ChessBoard Settings
-    [Header("Chess Board Settings")] 
+    [Header("Chess Board Settings")]
     [SerializeField, Tooltip("Chess board width.")]
     private int _boardWidth = 8;
     [SerializeField, Tooltip("Chess board depth.")]
@@ -25,19 +25,19 @@ public class BoardManager : MonoService
 
     // BoardSquare Settings
     [Header("Board Squares")]
-    [SerializeField, Tooltip("Prefab that is used for the board squares.")] 
+    [SerializeField, Tooltip("Prefab that is used for the board squares.")]
     private GameObject _squareBoardPrefab;
     [SerializeField, Tooltip("Materials for the board squares")]
     private Material[] _materialsArray;
-    
+
     // Chess pieces information
-    [Header("Chess Pieces Information and Properties")] 
+    [Header("Chess Pieces Information and Properties")]
     [SerializeField, Tooltip("Pawn Chess Piece")]
     private GameObject _pawnPrefab;
-    
+
     // Tweens
     private Tween _bounceAnimate;
-    
+
     // Services
     private EasyService<GameStateManager> _gameStateManager;
 
@@ -47,19 +47,19 @@ public class BoardManager : MonoService
         _boardSquares = new List<BoardSquare>();
         // Chess Board Initialization
         GenerateChessBoard();
-        
+
         // Pawn creation for testing
         CreatePiece(ChessPieceType.Pawn, 4, 4, Team.Friendly);
-        
+
     }
-    
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
             CreatePiece(ChessPieceType.Pawn, IndexCode.A8, Team.Friendly);
         }
-        
+
         if (Input.GetKeyDown(KeyCode.M))
         {
             GetTile(IndexCode.A8).ChessPieceAssigned.MoveToBlock(GetTile(IndexCode.H1));
@@ -96,10 +96,10 @@ public class BoardManager : MonoService
                 boardSquareComponent.IndexX = i;
                 boardSquareComponent.IndexZ = j;
                 boardSquareComponent.SetIndexCodeFromCartesian();
-                
+
                 // Add the newly created squareBoard to the list containing all board squares
                 _boardSquares.Add(boardSquareComponent);
-                
+
                 // Add material to the square board
                 MeshRenderer meshRenderer = squareBoard.GetComponent<MeshRenderer>();
                 if (materialIteration >= _materialsArray.Length)
@@ -107,67 +107,75 @@ public class BoardManager : MonoService
                 Material materialToApply = _materialsArray[materialIteration];
                 meshRenderer.material = materialToApply;
                 materialIteration++;
-                
+
                 // Update new X position
                 squareBoardPosition.x += squareBoardSize.x;
             }
             // Reset X position
             squareBoardPosition.x = firstSquarePosition.x;
-            
+
             // Update Z position
             squareBoardPosition.z += squareBoardSize.z;
         }
     }
-    
+
     // --------------- Public Functions and Methods ---------------
-    
+
     /// <summary>
     /// Get a tile with the specified position.
     /// </summary>
     /// <param name="x">The file of the tile.</param>
     /// <param name="y">The rank of the tile.</param>
     /// <returns>The tile at the specified location.</returns>
-    /// <exception cref="IndexOutOfRangeException">
-    /// The position is outside the chessboard, or the tile does not exist.
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The position is outside the chessboard.
+    /// </exception>
+    /// <exception cref="NullReferenceException">
+    /// The tile does not exist.
     /// </exception>
     public BoardSquare GetTile(int x, int y)
-    { 
-        if (IndexOutsideBounds(x, y))
-            throw new IndexOutOfRangeException($"Tile (${x}, ${y}) is out of range");
+    {
+        CheckIndex(x, y);
 
         BoardSquare querySquareBoard =
             _boardSquares.Find((square => square.IndexX == x && square.IndexZ == y));
 
         // If for some reason the square board does not exist, notify user and return a null object
         if (querySquareBoard == null)
-            throw new IndexOutOfRangeException($"Tile object at (${x}, ${y}) does not exist");
+            throw new NullReferenceException($"Tile object at (${x}, ${y}) does not exist");
 
         return querySquareBoard;
     }
-    
-    
+
+
     /// <summary>
     /// Get a tile with the specified position.
     /// </summary>
     /// <param name="code">The position code of the tile.</param>
     /// <returns>The tile at the specified location.</returns>
-    /// <exception cref="IndexOutOfRangeException">
-    /// The position is invalid, or the tile does not exist.
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The position is outside the chessboard.
+    /// </exception>
+    /// <exception cref="NullReferenceException">
+    /// The tile does not exist.
     /// </exception>
     public BoardSquare GetTile(IndexCode code)
         => GetTile((int)code % 8, (int)code / 8);
-    
-    
+
+
     /// <summary>
     /// Get a tile with the specified position.
     /// </summary>
     /// <param name="tile">The position of the tile.</param>
     /// <returns>The tile at the specified location.</returns>
-    /// <exception cref="IndexOutOfRangeException">
-    /// The position is outside the chessboard, or the tile does not exist.
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The position is outside the chessboard.
     /// </exception>
-    public BoardSquare GetTile((int, int) tile)
-        => GetTile(tile.Item1, tile.Item2);
+    /// <exception cref="NullReferenceException">
+    /// The tile does not exist.
+    /// </exception>
+    public BoardSquare GetTile((int x, int y) tile)
+        => GetTile(tile.x, tile.y);
 
     /// <summary>
     /// Create a chess piece at the specified location.
@@ -177,7 +185,7 @@ public class BoardManager : MonoService
     /// <param name="team">The faction of the piece.</param>
     /// <returns>The newly created chess piece.</returns>
     /// <exception cref="ArgumentOutOfRangeException">
-    /// The piece type or team is invalid.
+    /// The piece type or team is invalid, or the position is outside the board.
     /// </exception>
     public ChessPiece CreatePiece(ChessPieceType type, (int, int) pos, Team team)
     {
@@ -206,7 +214,7 @@ public class BoardManager : MonoService
             default:
                 throw new ArgumentOutOfRangeException(nameof(type), type, null);
         }
-        
+
         if (piece == null)
             throw new NullReferenceException($"Prefab for piece type ${type} is broken");
 
@@ -214,7 +222,7 @@ public class BoardManager : MonoService
         piece.gameObject.transform.position = square.CenterSurfaceTransform.position;
         piece.Team = team;
         square.ChessPieceAssigned = piece;
-        
+
         //TODO: More initialisation required on the chess piece
         _pieces.Add(pos, piece);
         return piece;
@@ -247,18 +255,142 @@ public class BoardManager : MonoService
     public ChessPiece CreatePiece(ChessPieceType type, int x, int y, Team team)
         => CreatePiece(type, (x, y), team);
 
+    /// <summary>
+    /// Get the piece at the tile specified.
+    /// </summary>
+    /// <param name="x">The file of the tile.</param>
+    /// <param name="y">The rank of the tile.</param>
+    /// <returns>The piece at the tile, or null if there is none.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The coordinates are outside the board.
+    /// </exception>
+    public ChessPiece GetPiece(int x, int y)
+    {
+        if (CheckIndex(x, y))
+            throw new ArgumentOutOfRangeException($"Tile (${x}, ${y}) is out of range");
+
+        if (_pieces.TryGetValue((x, y), out var piece))
+        {
+            return piece;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Get the piece at the tile specified.
+    /// </summary>
+    /// <param name="pos">The position of the tile.</param>
+    /// <returns>The piece at the tile, or null if there is none.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The coordinates are outside the board.
+    /// </exception>
+    public ChessPiece GetPiece((int x, int y) pos)
+        => GetPiece(pos.x, pos.y);
+
+    /// <summary>
+    /// Move a piece to the specified tile.
+    /// </summary>
+    /// <param name="src">The tile of the chess piece.</param>
+    /// <param name="dst">The tile to move to.</param>
+    /// <returns>Whether the move succeeded.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// The piece to move does not exist.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Either of the positions are outside the board.
+    /// </exception>
+    public bool MovePiece((int x, int y) src, (int, int) dst)
+    {
+        CheckIndex(src);
+        CheckIndex(dst);
+
+        var piece = GetPiece(src) ?? throw new ArgumentNullException(
+                $"attempt to move nonexistent piece at (${src.x}, ${src.y})"
+            );
+
+        // check for existing piece at destination
+        var capture = GetPiece(dst);
+        if (capture != null)
+        {
+            // destination is occupied, try to capture
+            if (capture.Team == piece.Team)
+                return false; // friendly fire is disallowed
+            _pieces.Remove(dst);
+        }
+
+        // do the logical move
+        _pieces.Remove(src);
+        _pieces.Add(dst, piece);
+
+        // TODO: update visual location and animate update
+
+        return true;
+    }
+
+    /// <summary>
+    /// Move a piece to the specified tile.
+    /// </summary>
+    /// <param name="src">The tile of the chess piece.</param>
+    /// <param name="x">The file of the tile to move to.</param>
+    /// <param name="y">The rank of the tile to move to.</param>
+    /// <returns>Whether the move succeeded.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// The piece to move does not exist.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Either of the positions are outside the board.
+    /// </exception>
+    public bool MovePiece((int, int) src, int x, int y)
+        => MovePiece(src, (x, y));
+
+    /// <summary>
+    /// Move a piece to the specified tile.
+    /// </summary>
+    /// <param name="x">The file of the chess piece.</param>
+    /// <param name="y">The rank of the chess piece.</param>
+    /// <param name="dst">The tile to move to.</param>
+    /// <returns>Whether the move succeeded.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// The piece to move does not exist.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Either of the positions are outside the board.
+    /// </exception>
+    public bool MovePiece(int x, int y, (int, int) dst)
+        => MovePiece((x, y), dst);
+
+    /// <summary>
+    /// Move a piece to the specified tile.
+    /// </summary>
+    /// <param name="srcX">The file of the chess piece.</param>
+    /// <param name="srcY">The rank of the chess piece.</param>
+    /// <param name="dstX">The file of the tile to move to.</param>
+    /// <param name="dstY">The rank of the tile to move to.</param>
+    /// <returns>Whether the move succeeded.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// The piece to move does not exist.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Either of the positions are outside the board.
+    /// </exception>
+    public bool MovePiece(int srcX, int srcY, int dstX, int dstY)
+        => MovePiece((srcX, srcY), (dstX, dstY));
+
     // --------------- Private Helpers ---------------
-    // Returns true if index is within the bounds of the chess board
-    private bool IndexWithinBounds(int xIndex, int zIndex)
-    {
-        return !(xIndex >= _boardDepth || zIndex >= _boardWidth);
-    }
-    
+
     // Returns true if the index is outside the bounds of the chess board
-    private bool IndexOutsideBounds(int xIndex, int zIndex)
+    private bool CheckIndex(int x, int y)
     {
-        return (xIndex >= _boardDepth || zIndex >= _boardWidth);
+        if (x >= _boardDepth || y >= _boardWidth)
+        {
+            throw new ArgumentOutOfRangeException($"Tile (${x}, ${y}) is out of range");
+        }
+        return false;
     }
+
+    private bool CheckIndex((int x, int y) pos)
+        => CheckIndex(pos.x, pos.y);
 
     //TEMP - DELETE THIS LATER
     private IEnumerator DelayedBounce()
