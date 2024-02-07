90fd118d061d7cbbf4a4411f6a7830e48cd20b84
David Alfred <dnalfred20@gmail.com>
2024-02-07 09:44:59 +0800

    Initial Store UI

---
+++ Assets/Scripts/StoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreScript : MonoBehaviour
+{
+//    public void StartGame()
+//    {
+//        Debug.log("Starting a new game")
+//    }
+
+    public void OpenStore()
+    {
+        Debug.log("Opening the store menu")
+    }
+    
+//    public void ExitGame()
+//    {
+//        Debug.log("Exiting the game")
+//    }
+}
