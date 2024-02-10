ad65b89b03187be8b790c0fa7b0a0b8b59822cae
Jeandre Viljoen <jeandre@renderheads.com>
2024-02-10 11:11:00 +0200

    Added Currency Manager with some basic required functions

---
+++ Assets/Scripts/Managers/CurrencyManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Services;
+using UnityEngine;
+
+public class CurrencyManager : MonoService
+{
+    public int Currency { get; private set; }
+
+    public event Action<int> OnCurrencyAdded; 
+    public event Action<int> OnCurrencyRemoved;
+
+     void Start()
+     {
+         Currency = GlobalGameAssets.Instance.StartCurrency;
+     }
+
+    /// <summary>
+    /// Adds currency.
+    /// </summary>
+    /// <param name="amount">Amount to add (Cannot be negative)</param>
+    public void AddCurrency(int amount)
+    {
+        if (amount < 0)
+        {
+            Debug.LogWarning($"[CurrencyManager.cs] - AddCurrency() : Cannot ADD negative currency, use TryRemoveCurrency() instead.");
+            return;
+        }
+        if (amount == 0)
+        {
+            Debug.LogWarning($"[CurrencyManager.cs] - AddCurrency() : Tried to add 0 currency");
+        }
+
+        Currency += amount;
+        OnCurrencyAdded?.Invoke(amount);
+    }
+    
+    /// <summary>
+    /// Removes currency if there is enough available. 
+    /// </summary>
+    /// <param name="amount">amount to remove (Cannot be negative)</param>
+    /// <returns>true if currency successfully removed</returns>
+    public bool TryRemoveCurrency(int amount)
+    {
+        if (amount < 0)
+        {
+            Debug.LogWarning($"[CurrencyManager.cs] - TryRemoveCurrency() : Cannot REMOVE negative currency, use AddCurrency() instead.");
+            return false;
+        }
+        if (!HasCurrency(amount))
+        {
+            Debug.LogWarning($"[CurrencyManager.cs] - TryRemoveCurrency() : Not enough available currency to remove. Returning early");
+            return false;
+        }
+        if (amount == 0)
+        {
+            Debug.LogWarning($"[CurrencyManager.cs] - TryRemoveCurrency() : Tried to remove 0 currency");
+        }
+
+        Currency -= amount;
+        OnCurrencyRemoved?.Invoke(amount);
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player has has enough currency given in parameters
+    /// </summary>
+    /// <param name="amount">Amount to check (Cannot be negative)</param>
+    /// <returns>true if has enough currency, otherwise false.</returns>
+    public bool HasCurrency(int amount)
+    {
+        if (amount < 0)
+        {
+            Debug.LogError($"[CurrencyManager.cs] - HasCurrency() : Cannot Check for negative currency. Returning false");
+            return false;
+        }
+        
+        if (amount <= Currency)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
--- Assets/Scripts/ScriptableObjects/ChessPieceData.cs
+++ Assets/Scripts/ScriptableObjects/ChessPieceData.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Global Game Asset", menuName = "Custom Assets/New Global Game Asset")]
 public class GlobalGameAssets : ScriptableObjectSingleton<GlobalGameAssets>
 {
-   
+   [Header("Currency")]
+   public int StartCurrency = 0;
 }
