05146d1e61ac30b0711bf314649eb709b6a416a0
Rohan Maheshwari <rhnmaheshwari@gmail.com>
2024-02-15 15:09:15 +0530

    Main Menu has Play, Pause, Shop and Quit

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,4 +10,20 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1); 
         Debug.Log("Loads Level 1"); //Loads Level 1
     }
+    
+    public void OnPauseGame()
+    {
+        Debug.Log("Pauses the Game"); //Game pauses
+    }
+
+    public void OnShop()
+    {
+        Debug.Log("Shop Menu Will Open"); // Shop Menu will open
+    }
+
+
+    public void OnCloseGame()
+    {
+        Debug.Log("Quits The Game"); //Quits the game
+    }
 }
