9811850bc7b3d01bc7942c67c6ac850e07a1de41
Rohan Maheshwari <rhnmaheshwari@gmail.com>
2024-02-15 21:32:00 +0530

    On Hitting Esc Key on SampleScene the MainMenu shows up

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Services;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoService
 {
@@ -13,6 +14,13 @@ public class MenuManager : MonoService
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) // On Hitting the escape key the MainMenu shows up
+        {
+            GoToMainMenu();
+        }
+    }
+    private void GoToMainMenu() // Loads the MainMenu
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
