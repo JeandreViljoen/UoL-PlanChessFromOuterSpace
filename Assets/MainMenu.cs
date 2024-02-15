3b6f928c45450e66c48e61ce54d7720a3b3f1bc0
Rohan Maheshwari <rhnmaheshwari@gmail.com>
2024-02-15 13:58:27 +0530

    implemented MainMenu.cs

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void OnPlayGame()
+    {
+        SceneManager.LoadSceneAsync(1); //Loads Level 1
+    }
+}
