ecd23cbfe3e4a4a93b3e4e2e362885a77f0ad1a7
Jeandre Viljoen <jeandre@renderheads.com>
2024-02-16 16:04:47 +0200

    Pause Menu Tween example code

--- Assets/Scripts/Managers/PauseMenu.cs
+++ Assets/Scripts/Managers/PauseMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,28 +7,42 @@ using DG.Tweening;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu;
-    [SerializeField] RectTransform pausemenupanel;
-    [SerializeField] float posTopY, posMidY; 
-    [SerializeField] float tweentime;
-    // Start is called before the first frame update
+    [SerializeField] private RectTransform _pauseMenuPanel;
+    [SerializeField] private Vector3 _pauseMenuPanelRelativeHideOffset;
+    private Vector3 _showPosition, _hidePosition;
+    [SerializeField] private float _tweenTime;
+
+    private Tween _tweenPauseMenuMove;
+
+    private void Awake()
+    {
+        _showPosition = _pauseMenuPanel.transform.localPosition;
+        _hidePosition = _showPosition + _pauseMenuPanelRelativeHideOffset;
+    }
+
+    private void Start()
+    {
+        _pauseMenuPanel.transform.localPosition = _hidePosition;
+        gameObject.SetActive(false);
+    }
+
     public void Pause()
     {
-        pauseMenu.SetActive(true);
+        gameObject.SetActive(true);
         Time.timeScale = 0;
         PauseMenuIn();
     }
-
     public void Home()
     {
         SceneManager.LoadScene(0);
+
         Time.timeScale = 1;
     }
 
     public void Resume()
     {
         PauseMenuOut();
-        pauseMenu.SetActive(false);
+        gameObject.SetActive(false);
         Time.timeScale = 1;
     }
 
@@ -39,11 +54,13 @@ public class PauseMenu : MonoBehaviour
 
     public void PauseMenuIn()
     {
-        pausemenupanel.DOAnchorPosY(posMidY, tweentime);
+        _tweenPauseMenuMove?.Kill();
+        _tweenPauseMenuMove = _pauseMenuPanel.gameObject.transform.DOLocalMove(_showPosition, _tweenTime).SetUpdate(true);
     }
 
     public void PauseMenuOut()
     {
-        pausemenupanel.DOAnchorPosY(posTopY, tweentime);
+        _tweenPauseMenuMove?.Kill();
+        _tweenPauseMenuMove = _pauseMenuPanel.gameObject.transform.DOLocalMove(_hidePosition, _tweenTime).SetUpdate(true);
     }
 }
