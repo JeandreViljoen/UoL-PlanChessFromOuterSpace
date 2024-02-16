394694566519fe29dd77caf90afd0c89ce4f120f
Rohan Maheshwari <rhnmaheshwari@gmail.com>
2024-02-16 14:56:47 +0530

    created PauseMenu for the sample scene

---
+++ Assets/Scripts/Managers/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    // Start is called before the first frame update
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        PauseMenuIn();
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        PauseMenuOut();
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void PauseMenuIn()
+    {
+
+    }
+
+    public void PauseMenuOut()
+    {
+
+    }
+}
