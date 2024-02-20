a5d817edbcf3f2aef59739ca8391be8c48909efb
Jeandre Viljoen <jeandre@renderheads.com>
2024-02-20 17:08:51 +0200

    Added AI Balance data object

---
+++ Assets/Scripts/ScriptableObjects/AIBalanceData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AIBalanceData", menuName = "Custom Assets/New AI Balance Data")]
+public class AIBalanceData : ScriptableObject
+{
+    [Header("Unit Target Values")] 
+    
+    public int KingValue;
+    public int QueenValue;
+    public int RookValue;
+    public int KnightValue;
+    public int BishopValue;
+    public int PawnValue;
+}
