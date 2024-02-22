80d63fa2e02867eddaa8f88fcadac77977a33a39
David Alfred <dnalfred20@gmail.com>
2024-02-22 13:51:33 +0800

    Implementation of Shop Menu
    Initial Shop Menu with buttons for purchase of pieces.

---
+++ Assets/Scripts/UI/ButtonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public Text PriceTxt;
+    public Text BuyTxt;
+    public GameObject ShopManager;
+
+    void Update()
+    {
+        PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2,ItemID].ToString();
+    }
+}
---
+++ Assets/Scripts/UI/ShopManagerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShopManagerScript : MonoBehaviour
+{
+    public CurrencyBalanceData cbd;
+    public int[,] shopItems = new int [6,6];
+    public Text currencyTxt;
+    private float currencyBalance;
+    public CurrencyManager cm;
+
+    void Start()
+    {
+        //currencyBalance = cbd.StartCurrency;
+        currencyBalance = cm.Currency;
+        currencyTxt.text = "Currency: "+currencyBalance.ToString();
+        
+        //Sets ItemIDs in the shop
+        shopItems[1,1] = 1; //Pawn
+        shopItems[1,2] = 2; //Knight
+        shopItems[1,3] = 3; //Bishop
+        shopItems[1,4] = 4; //Rook
+        shopItems[1,5] = 5; //Queen
+        
+        //Extracts item prices to display in the shop
+        shopItems[2,1] = cbd.PawnCost;
+        shopItems[2,2] = cbd.KnightCost;
+        shopItems[2,3] = cbd.BishopCost;
+        shopItems[2,4] = cbd.RookCost;
+        shopItems[2,5] = cbd.QueenCost;     
+    }
+    
+    //Enables purchase of new units and updating of currency balance
+    public void Buy()
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+        
+        if(currencyBalance >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        {
+            currencyBalance -= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
+            currencyTxt.text = "Currency: $"+currencyBalance.ToString();
+            
+            //TO BE DONE
+            //add insertion of selected game piece on the board
+            //add updating of CurrencyManager Currency
+            //add exiting from Shop Menu (with or without changes)
+        }
+    }
+}
