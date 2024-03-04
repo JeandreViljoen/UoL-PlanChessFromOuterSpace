ce0408ecd8410a07272417081f8c5e6868c2bbb8
Rohan Maheshwari <rhnmaheshwari@gmail.com>
2024-03-04 15:42:01 +0530

    Setup and design of the tutorial functionality

---
+++ Assets/Scripts/HelpButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class HelpButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+public void Helptext()
+    {
+        Debug.Log("I am Help");
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+    
+}
