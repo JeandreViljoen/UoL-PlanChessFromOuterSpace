e59242482da087c7b05827f25df145a2a5e362ce
Nathan Du <nathandu@outlook.com>
2024-03-06 16:56:09 +0800

    AI: extract position scoring method
    Extracts the position scoring method, taking the chess piece itself
    and the destination square as parameter. This makes it easier to
    implement changes to scoring.

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -11,6 +11,8 @@ public class AI : MonoService
 
     private EasyService<BoardManager> currentBoard;
 
+    private AIBalanceData Balance => GlobalGameAssets.Instance.AIBalanceData;
+
     /// <summary>
     /// Restart AI computation.
     /// </summary>
@@ -20,38 +22,33 @@ public class AI : MonoService
         this.moveQueue = moveQueue;
     }
 
-    private int ScoreCapture(ChessPieceType pieceType, AIBalanceData scores)
+    private int ScoreCapture(ChessPieceType pieceType)
     {
         switch (pieceType)
         {
-            case ChessPieceType.Pawn: return scores.PawnValue;
-            case ChessPieceType.Knight: return scores.KnightValue;
-            case ChessPieceType.Bishop: return scores.BishopValue;
-            case ChessPieceType.Rook: return scores.RookValue;
-            case ChessPieceType.Queen: return scores.QueenValue;
-            case ChessPieceType.King: return scores.KingValue;
+            case ChessPieceType.Pawn: return Balance.PawnValue;
+            case ChessPieceType.Knight: return Balance.KnightValue;
+            case ChessPieceType.Bishop: return Balance.BishopValue;
+            case ChessPieceType.Rook: return Balance.RookValue;
+            case ChessPieceType.Queen: return Balance.QueenValue;
+            case ChessPieceType.King: return Balance.KingValue;
         }
 
         return 0;
     }
 
-    private struct Move : IComparable<Move> {
-        public BoardSquare destination;
-        public int score;
+    private int Score(ChessPiece piece, BoardSquare dst)
+    {
+        int score = 0;
 
-        public Move(BoardSquare destination, int score) {
-            this.destination = destination;
-            this.score = score;
-        }
-
-        public int CompareTo(Move other)
+        // add capture score
+        var capture = dst.ChessPieceAssigned;
+        if (capture)
         {
-            int result = score.CompareTo(other.score);
-            if (result != 0) return result;
-            result = destination.IndexX.CompareTo(other.destination.IndexX);
-            if (result != 0) return result;
-            return destination.IndexZ.CompareTo(other.destination.IndexZ);
+            score = ScoreCapture(capture.PieceType);
         }
+
+        return score;
     }
 
     /// <summary>
@@ -66,20 +63,11 @@ public class AI : MonoService
         var piece = moveQueue.First();
         var destinations = piece.GetAllPossibleMovesetTiles();
 
-        var balanceData = GlobalGameAssets.Instance.AIBalanceData;
-
         var moves = new SortedSet<Move>();
 
-        var list = "";
         foreach (var dst in destinations)
         {
-            var capture = dst.ChessPieceAssigned;
-            int score = 0;
-
-            if (capture)
-            {
-                score = ScoreCapture(capture.PieceType, balanceData);
-            }
+            int score = Score(piece, dst);
 
             moves.Add(new Move(dst, score));
         }
@@ -88,5 +76,26 @@ public class AI : MonoService
         var top = moves.Reverse().TakeWhile(m => m.score == highscore).ToArray();
 
         return top[Random.Range(0, top.Length)].destination;
+    }
+
+    private struct Move : IComparable<Move>
+    {
+        public BoardSquare destination;
+        public int score;
+
+        public Move(BoardSquare destination, int score)
+        {
+            this.destination = destination;
+            this.score = score;
+        }
+
+        public int CompareTo(Move other)
+        {
+            int result = score.CompareTo(other.score);
+            if (result != 0) return result;
+            result = destination.IndexX.CompareTo(other.destination.IndexX);
+            if (result != 0) return result;
+            return destination.IndexZ.CompareTo(other.destination.IndexZ);
+        }
     }
 }
