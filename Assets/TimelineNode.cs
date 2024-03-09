feb92278e48a076c0dc45acf4082ead01a8d9ee9
Jeandre Viljoen <jeandre@renderheads.com>
2024-03-09 19:25:36 +0200

    Added range icons to Timeline nodes

--- Assets/TimelineNode.cs
+++ Assets/TimelineNode.cs
@@ -14,6 +14,7 @@ public class TimelineNode : MonoBehaviour
     [SerializeField] private Image _portraitBorder;
     [SerializeField] private Image _portrait;
     [SerializeField] private TimelineSpeedIcons _speedIcons;
+    [SerializeField] private TimelineRangeIcons _rangeIcons;
     [SerializeField] private TextMeshProUGUI _levelText;
     [SerializeField] private GameObject _panelsContainer;
      private Vector3 _panelsContainerBasePosition;
@@ -61,6 +62,7 @@ public class TimelineNode : MonoBehaviour
     public void RefreshPiece()
     {
         _speedIcons.IconsToShow = Piece.Speed;
+        _rangeIcons.IconsToShow = Piece.Range;
         _levelText.text = $"LEVEL {Piece.Level}";
     }
 
@@ -69,6 +71,7 @@ public class TimelineNode : MonoBehaviour
         Piece.TimelineNode = this;
         _portrait.sprite = Piece.Portrait;
         _speedIcons.IconsToShow = Piece.Speed;
+        _rangeIcons.IconsToShow = Piece.Range;
         _levelText.text = $"LEVEL {Piece.Level}";
         _levelText.color = Piece.Team == Team.Friendly
             ? GlobalGameAssets.Instance.HighlightColor
---
+++ Assets/TimelineRangeIcons.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimelineRangeIcons : MonoBehaviour
+{
+    public LayoutGroup Layout;
+    public List<Image> RangeIcons;
+
+    private int _iconsToShow;
+    public int IconsToShow
+    {
+        get
+        {
+            return _iconsToShow;
+        }
+        set
+        {
+            _iconsToShow = value;
+            SetIcons(_iconsToShow);
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetIcons(int value)
+    {
+        for (int i = 0; i < RangeIcons.Count; i++)
+        {
+            if (i < value)
+            {
+                RangeIcons[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                RangeIcons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
